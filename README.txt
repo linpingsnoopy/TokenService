66c89559b8d349ae4811a5441f016a5710be887c
linpingsnoopy <linpingsnoopy@gmail.com>
2022-11-27 19:44:07 +1300

    add more test

--- README.txt
+++ README.txt
@@ -3,5 +3,8 @@
 
 2. Send the request example:
     curl -H "Content-Type: application/json" -X POST -d '["4444-4444-4444-4444"]' http://localhost:3000/tokenize
-    curl -H "Content-Type: application/json" -X POST -d '["ASDFGWERTSXCVXCddd"]' http://localhost:3000/detokenize
+    curl -H "Content-Type: application/json" -X POST -d '["He6zdCh1ReYxzZ9EKRawsi_O_aRykQTV"]' http://localhost:3000/detokenize
     curl -H "Content-Type: application/json" -X GET http://localhost:3000/accounts
+
+3. Run the test
+    stop the 'npm start' terminal, and then run 'npm test'
--- src/test/app.test.js
+++ src/test/app.test.js
@@ -8,33 +8,81 @@ describe('/accounts', function() {
     return request(app)
       .get('/accounts')
       .set('Accept', 'application/json')
-     // .expect('Content-Type', /json/)
       .expect(200)
       .then(response => {
-        expect(response.body).toEqual([ 'He6zdCh1ReYxzZ9EKRawsi_O_aRykQTV', 'fvMymE7X0Je1IzMDgWooV5iGBPw0yoFy']);
+        expect(response.body).toEqual([ "He6zdCh1ReYxzZ9EKRawsi_O_aRykQTV", "fvMymE7X0Je1IzMDgWooV5iGBPw0yoFy"]);
       });
     });
 
-    it('POST : /tokenize swap a collection of account numbers for tokens', function() {
+    it('POST : /tokenize swap a account numbers for a token', function() {
       return request(app)
         .post('/tokenize')
         .set('Content-Type', 'application/json')
         .send(["4444-4444-4444-4444"])
         .expect(200)
         .then(response => {
-          expect(response.body).toEqual(['He6zdCh1ReYxzZ9EKRawsi_O_aRykQTV']);
+          expect(response.body).toEqual(["He6zdCh1ReYxzZ9EKRawsi_O_aRykQTV"]);
         });
     });
 
-    it('POST : /detokenize swap a collection of tokens numbers for accounts', function() {
+    it('POST : /tokenize new account should return 200 and create record in db', function() {
+      return request(app)
+        .post('/tokenize')
+        .set('Content-Type', 'application/json')
+        .send(["2222-2222-2222-2222"])
+        .expect(200)
+        .then(response => {
+          expect(response.body.length).toEqual(1);
+        });
+    });
+
+    it('POST : /tokenize swap a collection of account numbers for tokens', function() {
+      return request(app)
+        .post('/tokenize')
+        .set('Content-Type', 'application/json')
+        .send(["4444-4444-4444-4444", "3333-3333-3333-3333"])
+        .expect(200)
+        .then(response => {
+          expect(response.body).toEqual(['He6zdCh1ReYxzZ9EKRawsi_O_aRykQTV','fvMymE7X0Je1IzMDgWooV5iGBPw0yoFy']);
+        });
+    });
+
+    it('POST : /tokenize invalid account should return 400', function() {
+      return request(app)
+        .post('/tokenize')
+        .set('Content-Type', 'application/json')
+        .send(["4444-4444-4444"])
+        .expect(400);
+    });
+
+    it('POST : /detokenize swap back a tokens for the original account number', function() {
       return request(app)
         .post('/detokenize')
         .set('Content-Type', 'application/json')
         .send(["He6zdCh1ReYxzZ9EKRawsi_O_aRykQTV"])
         .expect(200)
         .then(response => {
-          expect(response.body).toEqual(['4444-4444-4444-4444']);
+          expect(response.body).toEqual(["4444-4444-4444-4444"]);
         });
+    });
+
+    it('POST : /detokenize swap back a collection of tokens numbers for accounts', function() {
+      return request(app)
+        .post('/detokenize')
+        .set('Content-Type', 'application/json')
+        .send(["He6zdCh1ReYxzZ9EKRawsi_O_aRykQTV", "fvMymE7X0Je1IzMDgWooV5iGBPw0yoFy"])
+        .expect(200)
+        .then(response => {
+          expect(response.body).toEqual(["4444-4444-4444-4444", "3333-3333-3333-3333"]);
+        });
+    });
+
+    it('POST : /detokenize token length should 32', function() {
+      return request(app)
+        .post('/detokenize')
+        .set('Content-Type', 'application/json')
+        .send(["He6zdCh1ReYxzZ9EKRawsi"])
+        .expect(400);
     });
 
 });
